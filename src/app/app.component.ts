1ff4f6361e7eefacef14f0702823c1f4560d74ba
TiagoBrum <tiago.brum@hevo.com.br>
2017-07-24 17:50:57 -0300

    criado base angular2

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: [
+  	'../../node_modules/bootstrap/dist/css/bootstrap.css',
+  	'../../node_modules/font-awesome/css/font-awesome.css',
+  	'./app.component.css',
+  ],
+})
+export class AppComponent {
+  title = 'teste';
+}
